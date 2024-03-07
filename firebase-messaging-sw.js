0aaebe235c1e71b7abeb4ce15ebdc255c4d07f60
dimaPolonez <dima-barxat@yandex.ru>
2024-03-07 08:50:32 +0300

    feat: add new files

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,8 @@
+importScripts('https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.1.3/firebase-messaging.js');
+
+firebase.initializeApp({
+    messagingSenderId: '523978386548'
+});
+
+const messaging = firebase.messaging();
